797d543eef45fc4fcfe2836026002818a196a0c8
Dejorden <d.moerman@hotmail.nl>
2023-05-15 15:40:43 +0200

    add arrow button click function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("Hij doet het");
+let arrowButtons = document.getElementsByClassName("arrow");
+
+for (let i = 0; i < arrowButtons.length; i++) {
+    arrowButtons[i].onclick = function () {
+
+    }
+}
