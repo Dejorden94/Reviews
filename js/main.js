e8680b365f8eac6ea1fc4b2931eaa153337655b7
Dejorden <d.moerman@hotmail.nl>
2023-05-15 16:10:36 +0200

    add button function for showing diffrent reviews

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 let arrowButtons = document.getElementsByClassName("arrow");
+let reviews = document.getElementsByClassName("review")
 
 for (let i = 0; i < arrowButtons.length; i++) {
     arrowButtons[i].onclick = function () {
-
+        reviews[0].style.display = "none";
+        reviews[1].style.display = "none";
+        reviews[2].style.display = "none";
+        reviews[3].style.display = "block";
+        reviews[4].style.display = "block";
+        reviews[5].style.display = "block";
     }
 }
